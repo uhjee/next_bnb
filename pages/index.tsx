2c0553733ed1ea02b8f6b3e524c110545d30b55c
uhjee <oper1490@gmail.com>
2022-05-21 16:23:17 +0900

    setting project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-size: 21px;
+  color: #333;
+`;
+
+const Index: React.FC = () => {
+  return <Container>hello </Container>;
+};
+
+export default Index;
